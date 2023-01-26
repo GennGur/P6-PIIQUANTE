70e8e78720960cd78f4035f266f3298595bf068b
Gourson Glenn <114163295+GennGur@users.noreply.github.com>
2023-01-26 18:41:36 +0100

    #

--- Back/app.js
+++ Back/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const mongoose = require ('mongoose');
 const app = express();
 const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
 const helmet = require('helmet');
+const path = require('path');
 
 require('dotenv').config();
 app.use(helmet({ crossOriginResourcePolicy: { policy: "same-site" } }));
@@ -24,5 +26,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/auth', userRoutes);
+app.use('/api/sauces', sauceRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
