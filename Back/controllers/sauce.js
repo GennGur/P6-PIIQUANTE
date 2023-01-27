ffa0d0afe324191ab3c88769606d7a0f11ea9525
Gourson Glenn <114163295+GennGur@users.noreply.github.com>
2023-01-27 15:27:03 +0100

    #

--- Back/controllers/sauce.js
+++ Back/controllers/sauce.js
@@ -25,3 +25,26 @@ exports.createSauce = (req, res, next) => {
         .then(() => {res.status(201).json({message: 'Sauce enregistrée !'})})
         .catch(error => res.status(400).json({error}))
 };
+
+exports.modifySauce = (req, res, next) => {
+    const sauceObject = req.file ? {
+      ...JSON.parse(req.body.sauce),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : {...req.body };
+
+    delete sauceObject._userId;
+    Sauce.findOne({_id: req.params.id})
+    .then((sauce) => {
+      if (sauce.userId != req.auth.userId) {
+        res.status(401).json({ message : 'Non-autorisé' });
+      } else {
+        Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id})
+        .then(() => res.status(200).json({message: 'Objet Modifié!'}))
+        .catch(error => res.status(401).json ({ error }));
+      }
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    })
+};
+
