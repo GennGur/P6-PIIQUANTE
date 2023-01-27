e22bac92da88e815b4fb89139104a10f9879b15a
Gourson Glenn <114163295+GennGur@users.noreply.github.com>
2023-01-27 15:52:57 +0100

    #

--- Back/controllers/sauce.js
+++ Back/controllers/sauce.js
@@ -48,3 +48,21 @@ exports.modifySauce = (req, res, next) => {
     })
 };
 
+exports.deleteSauce = (req, res, next) => {
+    Sauce.findOne({_id: req.params.id})
+    .then(sauce => {
+      if (sauce.userId != req.auth.userId) {
+        res.status(401).json({message: 'Non-autorisé'});
+      } else {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          Sauce.deleteOne({_id: req.params.id})
+            .then(() => {res.status(200).json({message: 'Objet supprimé !'})})
+            .catch(error => res.status(401).json({ error }));
+        });
+      }
+    })
+    .catch( error => {
+      res.status(500).json({ error });
+    });
+};
